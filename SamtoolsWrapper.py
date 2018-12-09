e3a4756ae5c384863b2eb5a788f09995ea581f6b
Erik Storrs <epstorrs@gmail.com>
2018-12-08 22:15:02 -0600

    bug fixes

--- SamtoolsWrapper.py
+++ SamtoolsWrapper.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 import sys
 from collections import OrderedDict
@@ -6,6 +7,60 @@ from multiprocessing import Pool
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+def worker_wrapper(args):
+        return execute_bam(*args)
+        
+def execute_bam(bam_fp, output_fp, operations_dict,
+                index_input_bam=False, verbose=True):
+    """Execute a bam file"""
+    # index bam if needed
+    if index_input_bam:
+        index_bam(bam_fp)
+
+    # execute remaining commands
+    command_args = generate_stream_commands(bam_fp, output_fp, operations_dict)
+    subprocess.check_output(command_args)
+
+    if verbose:
+        eprint(f'{bam_fp} completed')
+
+    return output_fp
+
+def generate_stream_commands(bam_fp, output_fp, operations_dict):
+    """
+    return command to run input bam through the given operations
+
+    """
+    command_args = []
+
+    items = iter(operations_dict.items())
+
+    # get first command
+    operation_identifier, operation_kwargs = next(items)
+    # if only one operation return output
+    if len(operations_dict) == 1:
+        return get_command_from_identifier(bam_fp, output_fp, operation_identifier, operation_kwargs)
+    else:
+        command_args += get_command_from_identifier(bam_fp, None, operation_identifier, operation_kwargs) + ['|']
+
+    c = len(operations_dict) - 1
+    for operation_identifier, operation_kwargs in items:
+        # if last operation, write an output file, otherwise stream to next operation
+        if c == 0:
+            command_args += get_command_from_identifier(None, output_fp, operation_identifier, operation_kwargs)
+        else:
+            command_args += get_command_from_identifier(None, None, operation_identifier, operation_kwargs) + ['|']
+            
+        c -= 1
+    return command_args
+
+def get_command_from_identifier(bam_fp, output_fp, identifier, kwargs):
+    """return command given identifier and operation kwargs"""
+    if identifier == 'sort':
+        return get_sort_command(bam_fp, output_fp, sort_threads=kwargs['sort_threads'])
+    if identifier == 'position_filter':
+        return get_position_filter_command(bam_fp, output_fp, kwargs['positions_fp'])
 
 def get_sort_command(bam_fp, output_fp, sort_threads=1):
     """get samtools sort command
@@ -19,7 +74,7 @@ def get_sort_command(bam_fp, output_fp, sort_threads=1):
     """
     samtools_command = ['samtools', 'sort']
 
-    if output_file is not None:
+    if output_fp is not None:
         samtools_command += ['-o', output_fp]
 
     if bam_fp is not None:
@@ -42,7 +97,7 @@ def get_position_filter_command(bam_fp, output_fp, positions_fp):
     """
     samtools_command = ['samtools', 'view', '-L', positions_fp]
 
-    if output_file is not None:
+    if output_fp is not None:
         samtools_command += ['-o', output_fp]
 
     if bam_fp is not None:
@@ -58,10 +113,10 @@ def index_bam(bam_fp):
     samtools_command = ['samtools', 'index', bam_fp]
     subprocess.check_output(samtools_command)
 
-
+    
 class SamtoolsWrapper(object):
     def __init__(self, input_files, output_dir, operations_dict,
-                 output_descriptor='samwrap', threads=1, verbose=True):
+                 output_descriptor='.samwrap', threads=1, verbose=True):
         """Wrapper for Samtools
         
         args:
@@ -102,75 +157,19 @@ class SamtoolsWrapper(object):
         
     def run_bams(self):
         # pull out index if it's in operations
-        v = self.operations_dict.get('index')
-        if v is not None:
+        if 'index' in self.operations_dict:
             index_input_bam = True
             del self.operations_dict['index']
         else:
             index_input_bam = False
-        
+                    
         # create argument pool
         arg_pool = []
         for fp in self.input_files:
             sample = fp.split('/')[-1]
             sample = re.sub(r'.bam', self.output_descriptor + '.bam', sample)
             output_fp = os.path.join(self.output_dir, sample)
-            arg_pool.append((fp, output_fp, index_input_bam))
+            arg_pool.append((fp, output_fp, self.operations_dict, index_input_bam, self.verbose))
         
-        with Pool(self.threads) as p:
-            p.map(self.worker_wrapper, arg_pool)
-        
-    def worker_wrapper(self, args):
-        return self.execute_bam(*args)
-        
-    def execute_bam(self, bam_fp, output_fp, index_input_bam=False):
-        """Execute a bam file"""
-        # index bam if needed
-        if index_input_bam:
-            index_bam(bam_fp)
-        
-        # execute remaining commands
-        command_args = self.generate_stream_commands(bam_fp, output_fp)
-        subprocess.check_output(command_args)
-        
-        if self.verbose:
-            eprint(f'{bam_fp} completed')
-        
-        return output_fp
-    
-    def generate_stream_commands(self, bam_fp, output_fp):
-        """
-        return command to run input bam through the given operations
-        
-        """
-        command_args = []
-        
-        items = self.operations_dict.items()
-        
-        # get first command
-        operation_identifier, operation_kwargs = next(items)
-        num_operations = len(self.operations_dict)
-        # if only one operation return output
-        if num_operations == 0:
-            return self.get_command_from_identifier(bam_fp, output_fp, operation_identifier, operation_kwargs)
-        else:
-            command_args += self.get_command_from_identifier(bam_fp, None, operation_identifier, operation_kwargs) + ['|']
-            
-        c = 0
-        for operation_identifier, operation_kwargs in items:
-            # if last operation, write an output file, otherwise stream to next operation
-            if num_operations - 1 == c:
-                command_args += self.get_command_from_identifier(None, output_fp, operation_identifier, operation_kwargs)
-            else:
-                command_args += self.get_command_from_identifier(None, None, operation_identifier, operation_kwargs) + ['|']
-        
-        return command_args
-    
-    @staticmethod
-    def get_command_from_identifier(bam_fp, output_fp, identifier, kwargs):
-        """return command given identifier and operation kwargs"""
-        if identifier == 'sort':
-            return get_sort_command(bam_fp, output_fp, sort_threads=kwargs['sort_threads'])
-        if identifier == 'position_filter':
-            return get_position_filter_command(bam_fp, output_fp, kwargs['positions_fp'])
-
+        with Pool(self.num_threads) as p:
+            p.map(worker_wrapper, arg_pool)
